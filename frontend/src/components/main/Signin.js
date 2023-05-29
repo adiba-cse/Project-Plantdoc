e30af7cb8101c439f3363cfdfa1447908e2d8845
Mohammad Mubassir <46520389+m-triple-m@users.noreply.github.com>
2023-05-29 13:36:55 +0530

    ..

--- frontend/src/components/main/Signin.js
+++ frontend/src/components/main/Signin.js
@@ -1,96 +1,111 @@
-import React from 'react'
+
+import { useFormik } from "formik";
+import React from "react";
+import Swal from "sweetalert2";
+
 const Signin = () => {
+  const loginForm = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+
+    onSubmit: async (values) => {
+      console.log(values);
+      const res = await fetch('http://localhost:5000/user/authenticate', {
+        method: 'POST',
+        body : JSON.stringify(values),
+        headers : {
+          'contet-Type' : 'application/json'
+  }
+});
+
+ if(res.status === 200){
+   Swal.fire({
+    icon : "success",
+    title : "success",
+    text : "Invalid Credentials"
+   })
+ }
+
+    },
+  });
+
   return (
-    <div>
-      <section className="vh-100" style={{ backgroundColor: "#eee" }}>
-  <div className="container h-100">
+    <section className="h-100 gradient-form" style={{ backgroundColor: "#eee" }}>
+  <div className="container py-5 h-100">
     <div className="row d-flex justify-content-center align-items-center h-100">
-      <div className="col-lg-12 col-xl-11">
-        <div className="card text-black" style={{ borderRadius: 25 }}>
-          <div className="card-body p-md-5">
-            <div className="row justify-content-center">
-              <div className="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
-                <p className="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">
-                  Sign in
-                </p>
-                <form className="mx-1 mx-md-4">
-                  <div className="d-flex flex-row align-items-center mb-4">
-                    <i className="fas fa-user fa-lg me-3 fa-fw" />
-                    <div className="form-outline flex-fill mb-0">
-                      <input
-                        type="text"
-                        id="form3Example1c"
-                        className="form-control"
-                      />
-                      <label className="form-label" htmlFor="form3Example1c">
-                        Your Name
-                      </label>
-                    </div>
-                  </div>
-                  <div className="d-flex flex-row align-items-center mb-4">
-                    <i className="fas fa-envelope fa-lg me-3 fa-fw" />
-                    <div className="form-outline flex-fill mb-0">
-                      <input
-                        type="email"
-                        id="form3Example3c"
-                        className="form-control"
-                      />
-                      <label className="form-label" htmlFor="form3Example3c">
-                        Your Email
-                      </label>
-                    </div>
-                  </div>
-                  <div className="d-flex flex-row align-items-center mb-4">
-                    <i className="fas fa-lock fa-lg me-3 fa-fw" />
-                    <div className="form-outline flex-fill mb-0">
-                      <input
-                        type="password"
-                        id="form3Example4c"
-                        className="form-control"
-                      />
-                      <label className="form-label" htmlFor="form3Example4c">
-                        Password
-                      </label>
-                    </div>
-                  </div>
-                  {/* <div className="d-flex flex-row align-items-center mb-4">
-                    <i className="fas fa-key fa-lg me-3 fa-fw" />
-                    <div className="form-outline flex-fill mb-0">
-                      <input
-                        type="password"
-                        id="form3Example4cd"
-                        className="form-control"
-                      />
-                      {/* <label className="form-label" htmlFor="form3Example4cd">
-                        Repeat your password
-                      </label> */}
-                    {/* </div>
-                  </div> */} 
-                  <div className="form-check d-flex justify-content-center mb-5">
+      <div className="col-xl-10">
+        <div className="card rounded-3 text-black">
+          <div className="row g-0">
+            <div className="col-lg-6">
+              <div className="card-body p-md-5 mx-md-4">
+                <div className="text-center">
+                  <img
+                    src="/images/flg_logo5147-removebg-preview.png"
+                    style={{ width: 250 }}
+                    alt="logo"
+                    shape
+                  />
+                  <h4 classNpngame="mt-1 mb-5 pb-1">Welcome To PlantDoc</h4>
+                </div>
+                <form>
+                  <p>Please login to your account</p>
+                  <div className="form-outline mb-4">
                     <input
-                      className="form-check-input me-2"
-                      type="checkbox"
-                      defaultValue=""
-                      id="form2Example3c"
+                      type="email"
+                      id="form2Example11"
+                      className="form-control"
+                      placeholder="Phone number or email address"
                     />
-                    <label className="form-check-label" htmlFor="form2Example3">
-                      I agree all statements in{" "}
-                      <a href="#!">Terms of service</a>
+                    <label className="form-label" htmlFor="form2Example11">
+                      Username
                     </label>
                   </div>
-                  <div className="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
-                    <button type="button" className="btn btn-primary btn-lg">
-                    Sign in
+                  <div className="form-outline mb-4">
+                    <input
+                      type="password"
+                      id="form2Example22"
+                      className="form-control"
+                    />
+                    <label className="form-label" htmlFor="form2Example22">
+                      Password
+                    </label>
+                  </div>
+                  <div className="text-center pt-1 mb-5 pb-1">
+                    <button
+                      className="btn btn-primary btn-block fa-lg gradient-custom-2 mb-3"
+                      type="button"
+                    >
+                      Log in
+                    </button>
+                    <a className="text-muted" href="#!">
+                      Forgot password?
+                    </a>
+                  </div>
+                  <div className="d-flex align-items-center justify-content-center pb-4">
+                    <p className="mb-0 me-2">Don't have an account?</p>
+                    <button type="button" className="btn btn-outline-danger">
+                      Create new
                     </button>
                   </div>
                 </form>
               </div>
-              <div className="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
+            </div>
+            <div className="col-lg-6 d-flex align-items-center gradient-custom-2">
+              <div className="text-white px-3 py-4 p-md-5 mx-md-4">
                 <img
-                  src="https://th.bing.com/th/id/R.e25abfee9b3fddc464db2eaf66bc4882?rik=DmjwJ9Ltw0nbbQ&riu=http%3a%2f%2fnwdistrict.ifas.ufl.edu%2fhort%2ffiles%2f2012%2f06%2fcercospora_hydrangea.jpg&ehk=65qzTpTiltaXC3JRYRTQq12Et5zNupIFZHXTG6i%2b4u4%3d&risl=&pid=ImgRaw&r=0"
+                  src="/images/flg_logo5147-removebg-preview.png"
                   className="img-fluid"
-                  alt=""
-                />
+                  alt="sample image"
+                  />
+                <h4 className="mb-4">We are more than just a company</h4>
+                <p className="small mb-0">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat.
+                </p>
               </div>
             </div>
           </div>
@@ -99,9 +114,7 @@ const Signin = () => {
     </div>
   </div>
 </section>
-     
-    </div>
-  )
-}
+  );
+};
 
-export default Signin
+export default Signin;
